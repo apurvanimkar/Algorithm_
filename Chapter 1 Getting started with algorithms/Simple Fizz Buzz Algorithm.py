12681d323cad348685934ac36901ea40899286e5
apurvanimkar <nimkarapurva2000@gmail.com>
2023-03-01 22:59:22 +0530

    first algo

---
+++ Chapter 1 Getting started with algorithms/Simple Fizz Buzz Algorithm.py
@@ -0,0 +1,11 @@
+l1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for i in l1:
+    
+    if(i%15==0):
+        print("Fizz-Buzz")
+    elif(i%3==0):
+        print("Fizz")
+    elif(i%5==0):
+        print("Buzz")
+    else:
+        print(i)
